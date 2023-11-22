9c2347e0f831520fb468eaa474f308195e1644cc
tkachenko <mykhailo.tkachenko@neptis.pl>
2023-11-22 18:43:47 +0100

    Fix room and java problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -73,8 +73,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
 
     // Room
-//    implementation("androidx.room:room-runtime:2.6.0")
-//    annotationProcessor("androidx.room:room-compiler:2.6.0")
-//    // To use Kotlin annotation processing tool ("kapt)
-//    kapt("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-runtime:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    // To use Kotlin annotation processing tool ("kapt)
+    kapt("androidx.room:room-compiler:2.6.0")
 }
